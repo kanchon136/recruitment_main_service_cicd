f71a245653852a97f3f7ac1d76af395a73362d4d
kanchon136 <miakanchon5100@gmail.ocm>
2026-08-17 17:08:18 +0600

    tenTimeTest

--- src/main/java/com/cd/recruitment_requisition_service/controller/Test.java
+++ src/main/java/com/cd/recruitment_requisition_service/controller/Test.java
@@ -1,4 +1,13 @@
 package com.cd.recruitment_requisition_service.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testing")
 public class Test {
+
+    public String testing(){
+        return  "Recruitment Service is Running with CICD pipiline";
+    }
 }
